dfe4e6dc180f0bba363fd55730387bfb5899078f
Kekskaempferin <73354770+Kekskaempferin@users.noreply.github.com>
2020-11-03 18:08:15 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
-function farbwahl(clr) {
+/*function farbwahl(clr) {
   document.getElementById("a").setBackground(Color.clr);
 }
+*/
 
+
+
+
+<script>
+function farbwahl() {
+  document.getElementById("a").innerHTML = "Hello World";
+}
