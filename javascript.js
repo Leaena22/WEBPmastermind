a335c3588d218af9d428fd08218e62587e301532
Lea <62146836+Leaena22@users.noreply.github.com>
2020-11-10 10:32:52 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -23,14 +23,15 @@ var farben = ['#1D70B7', '#E30513', '#F9B233', '#008D36', '#A2195B', '#44B8B3']
 // Funktion neues Spiel
 function gameSetup () {
     erstellenMagicCode(0, 6);
+};
 
 /* funktionierende Funktion für Auswahl der Farbe für Dot a */
 function farbwahl(clr) {
   document.getElementById("a").style.background = clr;
-}
+};
 
  // Funktion zum erstellen des zu erratenden Farbcodes
 function erstellenMagicCode (min, max) {
     for (var i = 0; i < 4; i++)
       code[i] = Math.floor(Math.random() * (max - min)) + min;
-  }
+  };
