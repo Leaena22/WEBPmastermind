3b099eea496ee593290b095996908cd9a536d0e7
Lea <62146836+Leaena22@users.noreply.github.com>
2020-11-10 11:45:00 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,7 +15,6 @@ function farbwahl() {
 }
 */
 
-
 var versuch = []; // Array für die Eingabezeile
 var magicCode = []; // Array für den zu erratenden Farbcode
 var farben = ['#1D70B7', '#E30513', '#F9B233', '#008D36', '#A2195B', '#44B8B3'];
@@ -25,15 +24,28 @@ function gameSetup () {
     erstellenMagicCode(0, 6);
 };
 
-/* funktionierende Funktion für Auswahl der Farbe für Dot a */
+// funktionierende Funktion für Auswahl der Farbe für Dot a
 function farbwahl(clr) {
   document.getElementById("a").style.background = clr;
 };
 
-// Funktion zum erstellen des zu erratenden Farbcodes
+/*// Funktion zum erstellen des zu erratenden Farbcodes *worked*
 function erstellenMagicCode (min, max) {
-    for (var i = 0; i < 4; i++)
-      magicCode[i] = farben[Math.floor(Math.random() * (max - min)) + min];
+    for (var i = 0; i < 1; i++)
+      magicCode[i] = farben[Math.floor(Math.random() * (max - min)) + min]; //+min macht keinen Unterschied weil unser min = 0
+  };*/
+
+function erstellenMagicCode () {
+    for (var i = 0; i < 1; i++)
+      magicCode[i] = farben[Math.floor(Math.random() * farben.length)];
   };
+
+// Funktion Vergleich Eingabe und magicCode
+function vergleich {
+  
+};
+
+
+
 
 gameSetup(); // Run the game
