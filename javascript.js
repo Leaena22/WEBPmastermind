50e395e5437d2e73ce49a34a4c1637ab5da767fb
Kekskaempferin <73354770+Kekskaempferin@users.noreply.github.com>
2020-11-17 11:04:38 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -53,7 +53,7 @@ function anSpeicher() {
   var row = table.insertRow(0);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
+  cell1.innerHTML = document.getElementsByClassName("steckplaetzeSpeicher");
   cell2.innerHTML = "NEW CELL2";
 };
 
