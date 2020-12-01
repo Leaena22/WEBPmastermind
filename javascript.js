7d4abfdd7c5b00f7c72f78d1ab2ff9ad2609bd6f
Lea <62146836+Leaena22@users.noreply.github.com>
2020-12-01 15:44:10 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -122,7 +122,7 @@ function runTime () {
 };
 
 function SpielStatus (status) {
-  gewonnen.className = status ;
+  document.getElementById('gewonnen').className = status ;
 } ;
 
 gameSetup(); // Run the game
